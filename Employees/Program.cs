de5b4ffba7c60d1192c1f1e79faeff34361bab94
Aslly Ivonne Zuñiga <126729392+AsllyZuniga@users.noreply.github.com>
2026-07-28 12:08:00 -0500

    feat(api): add RESTful endpoints for employees and contracts

--- Employees/Program.cs
+++ Employees/Program.cs
@@ -4,20 +4,16 @@ using Employees.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 
 builder.Services.AddApplication();
 
 builder.Services.AddInfrastructure(builder.Configuration);
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
